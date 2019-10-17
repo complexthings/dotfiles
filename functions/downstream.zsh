2d2a81bb2acc00deef7fc79f88e784380168390c
Greg Harvell <greg@blueacorn.com>
2019-10-17 12:29:25 -0400

    Updates

--- functions/downstream.zsh
+++ functions/downstream.zsh
@@ -2,8 +2,8 @@ downstream-dotfiles() {
     __echo_black "git clone git@github.com:complexthings/dotfiles.git ~/dotfiles-temp"
     git clone git@github.com:complexthings/dotfiles.git ~/dotfiles-temp
 
-    __echo_black "mv ~/dotfiles-tmp/.dotfiles ~/.dotfiles"
-    mv ~/dotfiles-tmp/.dotfiles ~/.dotfiles
+    __echo_black "mv ~/dotfiles-tmp ~/.dotfiles"
+    mv ~/dotfiles-tmp ~/.dotfiles
 
     __echo_black "rm -rf ~/dotfiles-tmp"
     rm -rf ~/dotfiles-tmp
